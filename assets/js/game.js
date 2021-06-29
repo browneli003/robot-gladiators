43cbb5a14dfd225260c82f07d8f30f072924846b
Elia Brown <brownelijah143@gmail.com>
2021-06-29 14:56:33 -0600

    add multiple opps

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,22 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
     console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+  
+    console.log(enemyNames[0]);
+    console.log(enemyNames[1]);
+    console.log(enemyNames[2]);
+    console.log(enemyNames.length);
+    for(var i = 0; i < enemyNames.length; i++) {
+        console.log(enemyNames[i]);
+        console.log(i);
+        console.log(enemyNames[i] + " is at " + i + " index");
+      }
 
-var fight = function() {
+var fight = function(enemyName) {
+//fight function statements
 // Alert players that they are starting the round
 window.alert("Welcome to Robot Gladiators!");
 
@@ -64,6 +75,10 @@ else {
   
 }
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
+
+//fight();
 
 
